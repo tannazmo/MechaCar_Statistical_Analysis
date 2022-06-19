46e592ed945e08a676334ae1f3f90cfcdccb63ca
tannazmo <100812091+tannazmo@users.noreply.github.com>
2022-06-18 23:11:13 -0400

    updated with deliverable_2 files.

--- MechaCarChallenge.R
+++ MechaCarChallenge.R
@@ -1,4 +1,12 @@
+# DELIVERABLE 1
 library(dplyr)
 df <- read.csv("MechaCar_mpg.csv", check.names = F, stringsAsFactors = F)
 lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=df)
 summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, data=df))
+
+
+#DELIVERABLE 2
+table <- read.csv(file='Suspension_Coil.csv', check.names = F, stringsAsFactors = F)
+total_summary <- table  %>% summarise(Mean=mean(PSI), Median = median(PSI), Variance= var(PSI), SD=sd(PSI), .groups=NULL)
+
+lot_summary <- table  %>% group_by(Manufacturing_Lot) %>% summarise(Mean=mean(PSI), Median = median(PSI), Variance= var(PSI), SD=sd(PSI), .groups=NULL)
